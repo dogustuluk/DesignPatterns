df3b737064c9309e2dd0813ce648701236b26ca5
dogustuluk <dogus.tuluk@gmail.com>
2022-05-11 13:49:05 +0300

    repositories folder created > IProductRepository created

---
+++ WebApp.StrategyDesignPattern/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.StrategyDesignPattern.Models;
+
+namespace WebApp.StrategyDesignPattern.Repositories
+{
+    interface IProductRepository
+    {
+        Task<Product> GetById(string id);
+        Task<List<Product>> GetAllByUserId(string userId);
+        Task<Product> Save(Product product);
+        Task<Product> Delete(Product product);
+        Task<Product> Update(Product product);
+    }
+}
