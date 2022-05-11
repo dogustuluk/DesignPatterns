76782db1bad7a0eb0f88a92f436cf48fcaf2719e
dogustuluk <dogus.tuluk@gmail.com>
2022-05-11 17:21:36 +0300

    EDatabaseType and Settings class are created

---
+++ WebApp.StrategyDesignPattern/Models/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.StrategyDesignPattern.Models
+{
+    public class Settings
+    {
+        public static string claimDatabaseType = "databasetype"; //bu tip bize her yerde lazım olacağı için "static" tutuyoruz.
+
+        public EDatabaseType databaseType;
+
+        //kullanıcı login olduğunda direkt olarak sql Server'a kaydetmesi için yazılan kod.
+        public EDatabaseType GetDefaultDatabaseType => EDatabaseType.SqlServer; //eğer lambda ile direkt olarak girersek bir property'nin sadece "get" ine karşılık gelir, set'i yoktur.
+
+    }
+}
