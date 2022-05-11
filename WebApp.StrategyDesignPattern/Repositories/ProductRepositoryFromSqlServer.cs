9464cbfabfe98179af50ef3719e8f42469543d51
dogustuluk <dogus.tuluk@gmail.com>
2022-05-11 14:03:00 +0300

    ProductRepositoryFromSqlServer GetAllByUserId method created

--- WebApp.StrategyDesignPattern/Repositories/ProductRepositoryFromSqlServer.cs
+++ WebApp.StrategyDesignPattern/Repositories/ProductRepositoryFromSqlServer.cs
@@ -1,4 +1,5 @@
 ﻿using BaseProject.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +20,14 @@ namespace WebApp.StrategyDesignPattern.Repositories
         {
             _context.Products.Remove(product); //Remove metodunun asenkronu yoktur. Nedeni ise remove metodunun işlemi gerçekleştirmeyip memory'de ilgili objenin
             //state'ini değiştirmesidir. alttaki kod olmadan yapılması istenen işlem veritabanına yansımamaktadır.
+            //property set eder.
 
             await _context.SaveChangesAsync();
         }
 
-        public Task<List<Product>> GetAllByUserId(string userId)
+        public async Task<List<Product>> GetAllByUserId(string userId)
         {
-            throw new NotImplementedException();
+            return await _context.Products.Where(x => x.UserId == userId).ToListAsync();
         }
 
         public Task<Product> GetById(string id)
