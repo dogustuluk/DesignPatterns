6a78e3f7b8648b2a31b98c03d2837b41660049b5
dogustuluk <dogus.tuluk@gmail.com>
2022-05-16 18:13:20 +0300

    UserCardTagHelper created

---
+++ WebApp.TemplateDesignPattern/UserCards/UserCardTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.TemplateDesignPattern.UserCards
+{
+    public class UserCardTagHelper : TagHelper
+    {//"UserCardTagHelper" >> TagHelper'dan önce verilen isim bizim 'tag helper'ımız olur.
+        public AppUser AppUser { get; set; }
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserCardTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+
+    }
+}
