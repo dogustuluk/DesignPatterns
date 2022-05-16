48b36b8409b936e00b2e079ba36e22787d0f62e3
dogustuluk <dogus.tuluk@gmail.com>
2022-05-16 18:05:33 +0300

    primeUserCardTemplate created

---
+++ WebApp.TemplateDesignPattern/UserCards/PrimeUserCardTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.TemplateDesignPattern.UserCards
+{
+    public class PrimeUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("<a href='#' class='card-link'>Mesaj Gönder</a>");
+            stringBuilder.Append("<a href='#' class='card-link'>Profil</a>");
+            return stringBuilder.ToString();
+            
+        }
+
+        protected override string SetPicture()
+        {
+            return $"  <img src='{AppUser.PictureUrl}' class='card-img-top'>";
+        }
+    }
+}
