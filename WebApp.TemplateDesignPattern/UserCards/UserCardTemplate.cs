69a370cb79c5594605baa5953233b2b699ad7b13
dogustuluk <dogus.tuluk@gmail.com>
2022-05-16 17:47:06 +0300

    UserCardTemplate > Build method created

--- WebApp.TemplateDesignPattern/UserCards/UserCardTemplate.cs
+++ WebApp.TemplateDesignPattern/UserCards/UserCardTemplate.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApp.TemplateDesignPattern.UserCards
@@ -14,6 +15,26 @@ namespace WebApp.TemplateDesignPattern.UserCards
         {
             AppUser = appUser;
         }
+        
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("<div class='card'>");
+            stringBuilder.Append(SetPicture());
+            //"@" işareti ile istenildiği gibi girinti verebiliriz.
+            stringBuilder.Append($@"<div class= 'card-body'>
+                                <h5>{AppUser.UserName}</h5>
+                                <p>{AppUser.Description}</p>");
+            stringBuilder.Append(SetFooter());
+            stringBuilder.Append("</div>");
+            stringBuilder.Append("</div>");
+
+            return stringBuilder.ToString();
+        }
+
 
         //protected kullanılır çünkü subclass'ların da bu method'lara erişmesini istiyoruz.
         protected abstract string SetFooter();
