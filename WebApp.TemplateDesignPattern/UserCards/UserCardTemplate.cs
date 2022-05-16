8c3cdac4bca270c0f7884d2e4907d9c59074f8bb
dogustuluk <dogus.tuluk@gmail.com>
2022-05-16 17:35:44 +0300

    UserCardTemplate created

---
+++ WebApp.TemplateDesignPattern/UserCards/UserCardTemplate.cs
@@ -0,0 +1,22 @@
+﻿using BaseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.TemplateDesignPattern.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+
+        public void SetUser(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        //protected kullanılır çünkü subclass'ların da bu method'lara erişmesini istiyoruz.
+        protected abstract string SetFooter();
+        protected abstract string SetPicture();
+    }
+}
