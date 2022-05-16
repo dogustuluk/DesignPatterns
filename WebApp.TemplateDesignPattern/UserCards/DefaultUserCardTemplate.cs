09f4f71ca8effdccf3ddb33a49fba9ec2055e19d
dogustuluk <dogus.tuluk@gmail.com>
2022-05-16 17:57:55 +0300

    DefaultUserCardTemplate created

---
+++ WebApp.TemplateDesignPattern/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.TemplateDesignPattern.UserCards
+{
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            return string.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            return $"  <img src='/userpictures/defaultUser.png' class='card-img-top'>";
+        }
+    }
+}
