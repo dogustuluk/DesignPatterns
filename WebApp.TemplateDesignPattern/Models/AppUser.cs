d60f7ab38959310e043cc2a6b80b639c2d79daea
dogustuluk <dogus.tuluk@gmail.com>
2022-05-16 16:56:44 +0300

    appUser edited

--- WebApp.TemplateDesignPattern/Models/AppUser.cs
+++ WebApp.TemplateDesignPattern/Models/AppUser.cs
@@ -8,5 +8,7 @@ namespace BaseProject.Models
 {
     public class AppUser : IdentityUser
     {
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
     }
 }
