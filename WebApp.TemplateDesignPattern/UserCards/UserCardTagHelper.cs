791cbca48c391f0b8ac641d1ee7d260cec94a8fd
dogustuluk <dogus.tuluk@gmail.com>
2022-05-21 14:20:02 +0300

    UserCardTagHelper > Process method created

--- WebApp.TemplateDesignPattern/UserCards/UserCardTagHelper.cs
+++ WebApp.TemplateDesignPattern/UserCards/UserCardTagHelper.cs
@@ -19,6 +19,42 @@ namespace WebApp.TemplateDesignPattern.UserCards
             _httpContextAccessor = httpContextAccessor;
         }
 
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate userCardTemplate;
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                userCardTemplate = new PrimeUserCardTemplate();
+            }
+            else
+            {
+                userCardTemplate = new DefaultUserCardTemplate();
+            }
+            userCardTemplate.SetUser(AppUser);//Build işleminin gerçekleşmesi için mutlaka verilmesi lazım.
+                                              //bunu istenildiği takdirde constructor'da da alabiliriz.
+
+            output.Content.SetHtmlContent(userCardTemplate.Build());
+        }
+        /*
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate userCardTemplate;
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                userCardTemplate = new PrimeUserCardTemplate();
+            }
+            else
+            {
+                userCardTemplate = new DefaultUserCardTemplate();
+            }
+            userCardTemplate.SetUser(AppUser);//Build işleminin gerçekleşmesi için mutlaka verilmesi lazım.
+                                              //bunu istenildiği takdirde constructor'da da alabiliriz.
+
+            output.Content.SetHtmlContent(userCardTemplate.Build());
+            return Task.CompletedTask; //asenkron olan method'ta asenkron kullanmadığımız için bunu yazabiliriz veya asenkron metodu override edebiliriz.
+            //üstte override gösteriliyor.
+        }
+        */
 
     }
 }
