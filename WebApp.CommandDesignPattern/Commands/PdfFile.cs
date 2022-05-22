908258b998ec9c53fe917019d0f73ad554476633
dogustuluk <dogus.tuluk@gmail.com>
2022-05-22 20:42:07 +0300

    PdfFile class created

---
+++ WebApp.CommandDesignPattern/Commands/PdfFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CommandDesignPattern.Commands
+{
+    public class PdfFile<T>
+    {
+        public readonly List<T> _list;
+        public string FileName => $"{typeof(T).Name}.pdf";
+        public string FileTypr => "application/octet-stream";
+    }
+}
