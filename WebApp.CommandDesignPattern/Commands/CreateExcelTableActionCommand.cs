b14021dc15e997925f2e5a46c16bfe972e8352cd
dogustuluk <dogus.tuluk@gmail.com>
2022-05-22 22:51:34 +0300

    ITableActionCommand, CreateExcelTableActionCommand and CreatePdfTableActionCommand classes are created

---
+++ WebApp.CommandDesignPattern/Commands/CreateExcelTableActionCommand.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CommandDesignPattern.Commands
+{
+    public class CreateExcelTableActionCommand<T> : ITableActionCommand
+    { //receiver alması gerekli (receiver > asıl işlem yapan class)
+        private readonly ExcelFile<T> _excelFile;
+        public CreateExcelTableActionCommand(ExcelFile<T> excelFile)
+        {
+            _excelFile = excelFile;
+        }
+        public IActionResult Execute()
+        {
+            //_excelFile'dan bize MemoryStream gelmektedir, artık bunu IActionResult'a dönüştürebiliriz.
+            var excelMemoryStream = _excelFile.Create();
+
+            //geriye download edilebilir bir dosya döndürmek istediğimizde "FileContentResul" kullanılabilir.
+            return new FileContentResult(excelMemoryStream.ToArray(), _excelFile.FileType) { FileDownloadName = _excelFile.FileName };
+        }
+    }
+}
---
+++ WebApp.CommandDesignPattern/Commands/CreatePdfTableActionCommand.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CommandDesignPattern.Commands
+{
+    public class CreatePdfTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly PdfFile<T> _pdfFile;
+        public CreatePdfTableActionCommand(PdfFile<T> pdfFile)
+        {
+            _pdfFile = pdfFile;
+        }
+        public IActionResult Execute()
+        {
+            var pdfFileMemoryStream = _pdfFile.Create();
+
+            return new FileContentResult(pdfFileMemoryStream.ToArray(), _pdfFile.FileType) {FileDownloadName = _pdfFile.FileName };
+        }
+    }
+}
