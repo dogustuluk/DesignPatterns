29a1318ae5643c2f23088e8099a879e9daa034b4
dogustuluk <dogus.tuluk@gmail.com>
2022-05-24 13:54:51 +0300

    FileCreateInvoker class created

---
+++ WebApp.CommandDesignPattern/Commands/FileCreateInvoker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CommandDesignPattern.Commands
+{
+    public class FileCreateInvoker
+    {
+        private ITableActionCommand _tableActionCommand; //readonly vermiyoruz burda çünkü >>> method''un içerisinde set ediyoruz.
+        //tüm command'leri "ITableActionCommand" üzerinden yaptığımız için alıyoruz.
+        private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>(); //"pdf ve excel oluştur" butonu veya "checkbox"tan
+                                                                                                 //seçili alanları da oluşturmamız için gerekli olan kod.
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);
+        }
+        public IActionResult CreateFile()
+        {
+            return _tableActionCommand.Execute();
+        }
+        public List<IActionResult> CreateFiles()
+        {
+            return tableActionCommands.Select(x => x.Execute()).ToList();
+        }
+    }
+}
