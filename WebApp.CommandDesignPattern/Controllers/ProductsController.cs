55f17d1bb7db595ac5128dea325b0b62abc58ccb
dogustuluk <dogus.tuluk@gmail.com>
2022-05-24 14:04:00 +0300

    ProductsController and views created

---
+++ WebApp.CommandDesignPattern/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CommandDesignPattern.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public readonly AppIdentityDbContext _context;
+        public ProductsController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+    }
+}
