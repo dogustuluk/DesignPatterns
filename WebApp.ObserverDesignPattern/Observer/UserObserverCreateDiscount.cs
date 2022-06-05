51e90d274eb6527ae27fa923b18c066853dc3107
dogustuluk <dogus.tuluk@gmail.com>
2022-06-05 23:54:49 +0300

    UserObserverCreateDiscount

---
+++ WebApp.ObserverDesignPattern/Observer/UserObserverCreateDiscount.cs
@@ -0,0 +1,27 @@
+﻿using BaseProject.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ObserverDesignPattern.Observer
+{
+    public class UserObserverCreateDiscount : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public UserObserverCreateDiscount(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverCreateDiscount>>();
+            var context = _serviceProvider.GetRequiredService<AppIdentityDbContext>();
+            context.Discounts.Add(new Models.Discount { Rate = 10, UserId = appUser.Id });
+            context.SaveChanges();
+            logger.LogWarning("Discount created");
+        }
+    }
+}
