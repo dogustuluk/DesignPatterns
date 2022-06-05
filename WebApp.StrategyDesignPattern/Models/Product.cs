7adce1bd7e51e8a2e0bbce95c7862795f21806f6
dogustuluk <dogus.tuluk@gmail.com>
2022-06-05 23:24:47 +0300

    observerPattern cloned to BaseProject

--- WebApp.StrategyDesignPattern/Models/Product.cs
+++ WebApp.StrategyDesignPattern/Models/Product.cs
@@ -1,11 +1,8 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApp.StrategyDesignPattern.Models
 {
