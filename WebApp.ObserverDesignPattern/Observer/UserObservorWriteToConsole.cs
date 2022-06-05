6da097aebfad4d0abef4d2fb4e5a833dc774dea6
dogustuluk <dogus.tuluk@gmail.com>
2022-06-05 23:45:19 +0300

    IUserObserver and UserObserverWritoToConsole classes are created

---
+++ WebApp.ObserverDesignPattern/Observer/UserObservorWriteToConsole.cs
@@ -0,0 +1,25 @@
+﻿using BaseProject.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ObserverDesignPattern.Observer
+{
+    public class UserObservorWriteToConsole : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObservorWriteToConsole(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObservorWriteToConsole>>();
+            logger.LogInformation($"User ID:{appUser.Id}");
+        }
+    }
+}
