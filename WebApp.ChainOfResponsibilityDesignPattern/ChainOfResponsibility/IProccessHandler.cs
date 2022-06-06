a3a40b3876fb15c0212201aa7750cc5e6e2e6de0
dogustuluk <dogus.tuluk@gmail.com>
2022-06-06 19:44:38 +0300

    IProccessHandle and ProccessHandle class are created

---
+++ WebApp.ChainOfResponsibilityDesignPattern/ChainOfResponsibility/IProccessHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ChainOfResponsibilityDesignPattern.ChainOfResponsibility
+{
+    public interface IProccessHandler
+    {
+        IProccessHandler SetNext(IProccessHandler proccessHandler); //bir sonraki halkanın varlığını kontrol etmeye yaramaktadır/ ne olacağını da belirler.
+
+        Object handle(Object o); //üzerine almış olduğumuz data üzerinde bir işlem yapıp, bir sonraki halkaya aktarmıyorsak geriye direkt olarak 
+                //>>> void dönebiliriz fakat biz burdaki örnekte geriye data döndürüp bir sonraki halkaya işlemi taşıyor olacağımız için Object alıp geriye de 
+                //Object dönüyoruz.
+    }
+}
---
+++ WebApp.ChainOfResponsibilityDesignPattern/ChainOfResponsibility/ProccessHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ChainOfResponsibilityDesignPattern.ChainOfResponsibility
+{
+    public abstract class ProccessHandler : IProccessHandler
+    {
+        private IProccessHandler nextProccessHandler;
+
+        //ProccessHandler metodunu aldığımızda aşağıdaki "handle" metodunu override etmiş olacağız. alt sınıfların uygulaması için >> virtual
+        public virtual object handle(object o) //virtual >>>alt sınıfların da ezilmesi için metodu "virtual" olarak işaretleriz.
+        {
+            if (nextProccessHandler != null)
+            {
+                return nextProccessHandler.handle(o);
+            }
+            return null;
+        }
+
+        public IProccessHandler SetNext(IProccessHandler proccessHandler)
+        {
+            nextProccessHandler = proccessHandler;
+            return nextProccessHandler;
+        }
+    }
+}
