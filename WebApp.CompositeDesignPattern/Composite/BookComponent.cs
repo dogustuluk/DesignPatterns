6e7e020e5d820b10c4c209d8157bd65512312cc4
dogustuluk <dogus.tuluk@gmail.com>
2022-06-11 22:05:35 +0300

    IComponent and BookComponent classes created

---
+++ WebApp.CompositeDesignPattern/Composite/BookComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CompositeDesignPattern.Composite
+{
+    public class BookComponent : IComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public BookComponent(string name, int id)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Count()
+        {
+            return 1; //desendeki dal-yaprak ilişkisini düşünürsek; burası dal kısmını temsil etmektedir. burası son noktadır, artık alt kategori bulunmaz
+        }
+
+        public string Display()
+        {
+            return $"<li class='list-group-item'>{Name}</li>";
+        }
+    }
+}
---
+++ WebApp.CompositeDesignPattern/Composite/IComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CompositeDesignPattern.Composite
+{
+    interface IComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        int Count(); //
+        string Display(); //ul tag'leri arasında bir kategori listesi yapıyor olucaz. Eğer cshtml kodları içerisinde hazırlamak istersek işlem oldukça 
+        //zor bir hale gelecektir. Dolayısıyla işi kolaylaştırmak için cs kısmında yapıyor olucaz bu listeleme kısmını. Bunu sağlaması için Display'i kullanırız.
+    }
+}
