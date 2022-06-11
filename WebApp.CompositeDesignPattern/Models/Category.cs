23aba974ff9280c473dbb71824621e5728533415
dogustuluk <dogus.tuluk@gmail.com>
2022-06-11 19:37:26 +0300

    Book and Category classes created

---
+++ WebApp.CompositeDesignPattern/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.CompositeDesignPattern.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+        public int ReferenceId { get; set; } //hangi kategorinin altında olduğunu belli etmek için kullanırız. örnek için aşağıdaki şemaya bak
+        public ICollection<Book> Books { get; set; }
+    }
+}
+
+// Id   Name    UserId      ReferenceId
+// 1    kitap   1           0           >>>>>>>>> 0 demek ana kategori anlamına gelmektedir.
+// 2    roman   1           1           >>>>>>>>> 1 numaralı kategorinin alt kategorisi olduğu anlamına gelmektedir.
