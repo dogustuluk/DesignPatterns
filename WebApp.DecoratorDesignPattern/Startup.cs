3ecbe6310786bc7fd218cb2bb1fc871ce05d60d9
dogustuluk <dogus.tuluk@gmail.com>
2022-07-06 19:21:38 +0300

    startup.cs and cshtml edit

--- WebApp.DecoratorDesignPattern/Startup.cs
+++ WebApp.DecoratorDesignPattern/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.DecoratorDesignPattern.Repositories;
 
 namespace BaseProject
 {
@@ -26,6 +27,7 @@ namespace BaseProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddDbContext<AppIdentityDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
