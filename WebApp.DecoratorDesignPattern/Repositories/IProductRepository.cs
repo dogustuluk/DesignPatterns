92e9b3f4ae12652d2244e491f3ea462c1113b903
dogustuluk <dogus.tuluk@gmail.com>
2022-07-06 18:38:59 +0300

    IProductRepository interface and ProductRepository class are created

---
+++ WebApp.DecoratorDesignPattern/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.DecoratorDesignPattern.Models;
+
+namespace WebApp.DecoratorDesignPattern.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetById(int id);
+        Task<List<Product>> GetAll();
+        Task<List<Product>> GetAll(string userId);
+        Task<Product> Save(Product product);
+        Task Update(Product product);
+        Task Remove(Product product);
+    }
+}
