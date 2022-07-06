596f257b510d2550f7cb7a944e97c6a0f7d12a07
dogustuluk <dogus.tuluk@gmail.com>
2022-07-06 09:56:49 +0300

    BookComposite created

---
+++ WebApp.CompositeDesignPattern/Composite/BookComposite.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.CompositeDesignPattern.Composite
+{
+    public class BookComposite : IComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Composite olduğundan dolayı bir list tutması gerekmektedir.
+        private List<IComponent> _components; //buradaki listeye ilgili component'leri ekleyecek ve silecek metotların da tanımlanması gerekmektedir.
+
+        public BookComposite(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void Add(IComponent component) //IComponent'i implemente eden bir obje almak zorundadır.
+        {
+            _components.Add(component);
+        }
+        public void Remove(IComponent component)
+        {
+            _components.Remove(component);
+        }
+
+        public int Count()
+        {
+            return _components.Sum(x => x.Count());
+        }
+
+        public string Display()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"<div class='text-primary my-1'><a href='#' class='menu'>{Name} ({Count()}) </a></div>");
+
+            if (!_components.Any()) return sb.ToString();
+
+            sb.Append("<ul class='list-group list-group-flush ml-3'>");
+
+            foreach (var item in _components)
+            {
+                sb.Append(item.Display()); //buraya kadar geldi ise BookComponent'teki display metodunu çağırır.
+            }
+
+            sb.Append("</ul>");
+
+            return sb.ToString();
+        }
+    }
+}
--- WebApp.CompositeDesignPattern/Composite/IComponent.cs
+++ WebApp.CompositeDesignPattern/Composite/IComponent.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace WebApp.CompositeDesignPattern.Composite
 {
-    interface IComponent
+    public interface IComponent
     {
         public int Id { get; set; }
         public string Name { get; set; }
