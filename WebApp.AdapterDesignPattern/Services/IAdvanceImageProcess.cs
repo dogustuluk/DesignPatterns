30827589ada70638a3e27b3d5ce37c1283d7fbd9
dogustuluk <dogus.tuluk@gmail.com>
2022-07-07 16:28:46 +0300

    AdvanceImageProcess and IAdvanceImageProcess are created

---
+++ WebApp.AdapterDesignPattern/Services/IAdvanceImageProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.AdapterDesignPattern.Services
+{
+    interface IAdvanceImageProcess
+    {
+        void AddWatermarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor);
+    }
+}
