163e613c8abfcb2b3b4a3f2eae3c223df161558f
dogustuluk <dogus.tuluk@gmail.com>
2022-07-07 15:45:43 +0300

    IImageProcess and ImageProcess class are created

---
+++ WebApp.AdapterDesignPattern/Services/ImageProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.AdapterDesignPattern.Services
+{
+    public class ImageProcess : IImageProcess
+    {
+        public void AddWatermark(string text, string filename, Stream imageStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
