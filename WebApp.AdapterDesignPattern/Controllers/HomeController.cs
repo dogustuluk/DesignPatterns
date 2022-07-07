7700a4bd00dedd9ca757f204c1dc13ba5321f10f
dogustuluk <dogus.tuluk@gmail.com>
2022-07-07 15:46:17 +0300

    HomeController >> AddWatermark methods are created

--- WebApp.AdapterDesignPattern/Controllers/HomeController.cs
+++ WebApp.AdapterDesignPattern/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.AdapterDesignPattern.Services;
 
 namespace BaseProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IImageProcess _imageProcess;
+        public HomeController(ILogger<HomeController> logger, IImageProcess imageProcess)
         {
             _logger = logger;
+            _imageProcess = imageProcess;
         }
 
         public IActionResult Index()
@@ -26,6 +30,26 @@ namespace BaseProject.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult AddWatermark()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWatermark(IFormFile image)
+        {
+            if (image is { Length: > 0 })
+            {
+                var imageMemoryStream = new MemoryStream();
+
+                await image.CopyToAsync(imageMemoryStream);
+
+                _imageProcess.AddWatermark("Adapter Design Pattern", image.FileName, imageMemoryStream);
+            }
+            return View();
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
